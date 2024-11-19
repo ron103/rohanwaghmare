f52c994b859c3c7c3a08424f41fc8a262eda9162
ron103 <waghmarerohan30@gmail.com>
2024-11-19 15:23:06 -0500

    updated intro

--- src/pages/Home/Intro.js
+++ src/pages/Home/Intro.js
@@ -21,21 +21,21 @@ function Intro() {
   return (
     <section id="intro">
       <div className="intro bg-primary flex flex-col items-center justify-center text-center relative">
-        <div className="h-[40vh] flex flex-col items-center justify-center gap-5 py-10 relative z-1">
-          <h1 className="name text-7xl lg:text-8xl text-fourth font-bold">
+        <div className="h-[40vh] min-h-[300px] lg:min-h-[50vh] flex flex-col items-center justify-center gap-6 px-5 py-10 relative z-1">
+          <h1 className="name text-6xl sm:text-5xl md:text-6xl lg:text-9xl xl:text-8xl text-fourth font-bold">
             Rohan Waghmare
           </h1>
-          <h1 className="tag text-3xl lg:text-4xl text-fifth font-semibold">
+          <h1 className="tag text-3xl sm:text-xl md:text-2xl lg:text-6xl text-fifth font-semibold">
             Persistence. Perseverance. Proficiency.
           </h1>
-
-          <p className="phrase text-fifth opacity-50 lg:text-sm font-mono w-[60%]">
-            I'm a technology enthusiast with a knack for developing projects that solve a problem. Passionate about learning and excelling, I seek to provide the most efficient solutions to any given complex problem.
+          <p className="phrase text-sm sm:hidden lg:text-lg xl:text-xl text-fifth opacity-50 font-mono max-w-[100%] sm:max-w-[100%] lg:max-w-[40%]">
+            I'm a technology enthusiast with a knack for developing projects that solve a problem. Passionate about learning and
+            excelling, I seek to provide the most efficient solutions to any given complex problem.
           </p>
-          <div className="flex gap-5">
+          <div className="flex flex-wrap gap-4">
             <button
               onClick={handleOpenResume}
-              className="border-[1px] mt-5 text-sm sm:text-sm font-mono border-secondary text-secondary px-10 py-2 rounded resume-button"
+              className="border border-secondary text-secondary px-6 sm:px-8 py-2 sm:py-3 rounded text-sm sm:text-base font-mono resume-button"
             >
               Resume / CV
             </button>
