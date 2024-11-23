945335d048669804ddd1d6cd63ebcd8b7930b178
ron103 <waghmarerohan30@gmail.com>
2024-11-23 17:06:47 -0500

    Update work.js

--- src/resources/work.js
+++ src/resources/work.js
@@ -21,12 +21,22 @@ export const work = [
     link: "https://careertools.binghamton.edu/organizations/tech-collective/",
     description: (
       <p>
-       <i className="ri-play-line"></i>Collaborated with a team of 6 to design, build, and maintain the official club website, utilizing HTML, CSS, JavaScript,
-and React to ensure a seamless user experience.
+       <i className="ri-play-line"></i>Worked on the development of the official club website using React.js and Firebase, achieving a 35% increase in user
+engagement, as measured through click-through rates and session duration, by incorporating improved UI/UX designs and
+real-time updates.
 
 <br></br>
-<i className="ri-play-line"></i>Managed and updated the backend of an ecommerce application, implementing efficient code and maintaining data integrity
-to enhance app performance and user satisfaction.
+<i className="ri-play-line"></i>Optimized backend services for an e-commerce platform using Node.js and Redis caching, reducing load times by 30% through the
+implementation of asynchronous processing and efficient data retrieval techniques.
+<br></br>
+<i className="ri-play-line"></i>Contributed to cross-platform development using Flutter, enabling the migration of features from a web application to Android and
+iOS platforms, which expanded platform accessibility and increased the user base by 25%.
+<br></br>
+<i className="ri-play-line"></i>Led sprint planning and collaborated with cross-functional teams in Agile/SCRUM settings, ensuring project milestones were met
+within deadlines while promoting team synergy.
+<br></br>
+<i className="ri-play-line"></i>Assisted in introducing CI/CD pipelines using GitHub Actions to automate deployment and basic integration testing, laying the
+foundation for a more efficient development workflow.
       </p>
     ),
   },
@@ -42,21 +52,19 @@ to enhance app performance and user satisfaction.
     link: "https://www.flowai.tech/",
     description: (
       <p>
-        <i className="ri-play-line"></i>Led and managed a team of 12 engineers, building a comprehensive data pipeline integrating web scraping, Kafka,
-Amazon S3, data transformation, PostgreSQL, AI models, and MongoDB, containerized with Docker.
+        <i className="ri-play-line"></i>Optimized backend applications using Django, improving system scalability and reducing API response times by 25% through
+efficient query handling and code optimization.
 <br></br>
-<i className="ri-play-line"></i>Developed web scrapers for Crunchbase, Pitchbook, LinkedIn, and CSV files to standardize and combine data from
-multiple sources, reducing inconsistency by 33%.
+<i className="ri-play-line"></i>Designed a robust data pipeline for aggregating data from Crunchbase, PitchBook, and LinkedIn into PostgreSQL, achieving a
+43% reduction in data inconsistencies through schema validation and efficient data integration workflows.
 <br></br>
-<i className="ri-play-line"></i>Implemented Kafka for data streaming, validated JSON data, and achieved 99% data integrity through schema validation
-and deduplication. Managed secure data backups in Amazon S3 with AES encryption, and designed storage solutions in
-Redshift Data Warehouse.
+<i className="ri-play-line"></i>Implemented secure RESTful APIs using OAuth2.0 for client authentication, ensuring data integrity and enhancing security.
 <br></br>
-<i className="ri-play-line"></i>Integrated and labeled data for AI model training, improving AI performance. Developed API calls for AI models using
-Django, enhancing backend functionality.
+<i className="ri-play-line"></i>Collaborated in a SCRUM team to refactor legacy codebases, applying object-oriented principles and design patterns, which
+improved code readability and maintainability.
 <br></br>
-<i className="ri-play-line"></i>Collaborated on cloud platform design and maintenance, contributing to reduced downtime. Conducted code reviews,
-testing, and troubleshooting, improving live production deployment efficiency.
+<i className="ri-play-line"></i>Preprocessed and managed large datasets stored in AWS S3 for AI model training, optimizing workflows for distributed systems
+and reducing processing time.
       </p>
     ),
   },
