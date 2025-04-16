a849e489758c8b855e1cad27b081dd9aa5879db9
ron103 <waghmarerohan30@gmail.com>
2025-04-16 16:46:34 -0400

    updated work exp

--- src/resources/work.js
+++ src/resources/work.js
@@ -3,34 +3,34 @@ import btc from '../components/logos/btc.png'
 import mihin from '../components/logos/mihin.jpeg'
 
 export const work = [
-  {
-    id: "630120134042e2d798b59dc4",
-    company: "Michigan Health Information Network",
-    title: "Software Engineering Apprentice",
-    period: "Mar 2025 - Present",
-    location: "Lansing, MI",
-    technologies: [
-      "Python",
-      "AWS",
-      "PostgreSQL",
-      "Terraform",
-    ],
-    image:
-      mihin,
-    link: "https://careertools.binghamton.edu/organizations/tech-collective/",
-    description: (
-      <p>
-       <i className="ri-play-line"></i>Developing a secure, HIPAA-compliant, serverless Inbox Messaging Platform using AWS Lambda (Python), API Gateway
-       (Cognito Authorizers), S3, and RDS (PostgreSQL) for healthcare communications and user settings.
+//   {
+//     id: "630120134042e2d798b59dc4",
+//     company: "Michigan Health Information Network",
+//     title: "Software Engineering Apprentice",
+//     period: "Mar 2025 - Present",
+//     location: "Lansing, MI",
+//     technologies: [
+//       "Python",
+//       "AWS",
+//       "PostgreSQL",
+//       "Terraform",
+//     ],
+//     image:
+//       mihin,
+//     link: "https://careertools.binghamton.edu/organizations/tech-collective/",
+//     description: (
+//       <p>
+//        <i className="ri-play-line"></i>Developing a secure, HIPAA-compliant, serverless Inbox Messaging Platform using AWS Lambda (Python), API Gateway
+//        (Cognito Authorizers), S3, and RDS (PostgreSQL) for healthcare communications and user settings.
 
-<br></br>
-<i className="ri-play-line"></i>Implementing backend logic in Python for efficient message filtering and ingestion workflows leveraging SNS, SQS, and
-DynamoDB.
-<br></br>
-<i className="ri-play-line"></i>Collaborating across engineering teams to ensure robust and compliant data pipelines within distributed AWS environments.
-      </p>
-    ),
-  },
+// <br></br>
+// <i className="ri-play-line"></i>Implementing backend logic in Python for efficient message filtering and ingestion workflows leveraging SNS, SQS, and
+// DynamoDB.
+// <br></br>
+// <i className="ri-play-line"></i>Collaborating across engineering teams to ensure robust and compliant data pipelines within distributed AWS environments.
+//       </p>
+//     ),
+//   },
   {
     id: "630120134042e2d798b59dc4",
     company: "Binghamton Tech Collective",
@@ -50,22 +50,13 @@ DynamoDB.
     link: "https://careertools.binghamton.edu/organizations/tech-collective/",
     description: (
       <p>
-       <i className="ri-play-line"></i>Worked on the development of the official club website using React.js and Firebase, achieving a 35% increase in user
-engagement, as measured through click-through rates and session duration, by incorporating improved UI/UX designs and
-real-time updates.
+       <i className="ri-play-line"></i>Developed the official club website using React.js and Firebase, boosting user engagement by 15%.
 
 <br></br>
-<i className="ri-play-line"></i>Optimized backend services for an e-commerce platform using Node.js and Redis caching, reducing load times by 30% through the
-implementation of asynchronous processing and efficient data retrieval techniques.
+<i className="ri-play-line"></i>Optimized performance of a Node.js e-commerce app by implementing caching strategies to reduce load times.
 <br></br>
-<i className="ri-play-line"></i>Contributed to cross-platform development using Flutter, enabling the migration of features from a web application to Android and
-iOS platforms, which expanded platform accessibility and increased the user base by 25%.
-<br></br>
-<i className="ri-play-line"></i>Led sprint planning and collaborated with cross-functional teams in Agile/SCRUM settings, ensuring project milestones were met
-within deadlines while promoting team synergy.
-<br></br>
-<i className="ri-play-line"></i>Assisted in introducing CI/CD pipelines using GitHub Actions to automate deployment and basic integration testing, laying the
-foundation for a more efficient development workflow.
+<i className="ri-play-line"></i>Built key features into a Swift-based iOS app for increased accessibility and cross-platform support.
+
       </p>
     ),
   },
@@ -81,19 +72,11 @@ foundation for a more efficient development workflow.
     link: "https://www.flowai.tech/",
     description: (
       <p>
-        <i className="ri-play-line"></i>Optimized backend applications using Django, improving system scalability and reducing API response times by 25% through
-efficient query handling and code optimization.
+        <i className="ri-play-line"></i>Improved Django backend performance by refactoring APIs and simplifying database queries.
 <br></br>
-<i className="ri-play-line"></i>Designed a robust data pipeline for aggregating data from Crunchbase, PitchBook, and LinkedIn into PostgreSQL, achieving a
-43% reduction in data inconsistencies through schema validation and efficient data integration workflows.
+<i className="ri-play-line"></i>Built a data ingestion pipeline in PostgreSQL for integrating company profiles from Crunchbase, PitchBook, and LinkedIn.
 <br></br>
-<i className="ri-play-line"></i>Implemented secure RESTful APIs using OAuth2.0 for client authentication, ensuring data integrity and enhancing security.
-<br></br>
-<i className="ri-play-line"></i>Collaborated in a SCRUM team to refactor legacy codebases, applying object-oriented principles and design patterns, which
-improved code readability and maintainability.
-<br></br>
-<i className="ri-play-line"></i>Preprocessed and managed large datasets stored in AWS S3 for AI model training, optimizing workflows for distributed systems
-and reducing processing time.
+<i className="ri-play-line"></i>Refactored legacy codebase using Docker containers for easier deployment in a SCRUM-based workflow.
       </p>
     ),
   },
