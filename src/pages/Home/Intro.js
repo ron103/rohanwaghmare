547eae04447db097e74dab9e86dd73712fda6f59
ron103 <waghmarerohan30@gmail.com>
2025-07-06 11:02:25 -0400

    Update resume

--- src/pages/Home/Intro.js
+++ src/pages/Home/Intro.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ResumePDF from "../../components/logos/RohanWaghmare-resume-J.pdf";
+import ResumePDF from "../../components/logos/Rohan-resume-SDESAP.pdf";
 
 function Intro() {
   const handleOpenResume = () => {
